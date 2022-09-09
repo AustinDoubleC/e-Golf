6b7b1dfd727c1d7b2090029cdf42d8314e801a4e
Austin <austinccy81@gmail.com>
2022-09-09 15:50:20 +0100

    Add carousel, chat, footer

---
+++ index.js
@@ -0,0 +1,32 @@
+btnChatOpen = document.getElementById("chatOpen")
+chatbox = document.getElementById("chatbox")
+btnChatClose = document.getElementById("chatClose")
+btnSendChat = document.getElementById("btnSendChat")
+chatInput = document.getElementById("chatInput")
+
+btnChatClose.addEventListener("click",()=>{
+    chatbox.style.transform = "translateX(600px)"
+})
+btnChatOpen.addEventListener("click",()=>{
+    chatbox.style.transform = "translateX(-600px)"
+})
+btnSendChat.addEventListener("click",()=>{
+    chatInput.value = ""
+})
+
+/*Carousel control*/
+let items = document.querySelectorAll('.carousel .carousel-item')
+
+		items.forEach((el) => {
+			const minPerSlide = 4;
+			let next = el.nextElementSibling
+			for (let i=1; i<minPerSlide; i++) {
+				if (!next) {
+            // wrap carousel by using first child
+            next = items[0]
+        }
+        let cloneChild = next.cloneNode(true)
+        el.appendChild(cloneChild.children[0])
+        next = next.nextElementSibling
+    }
+})
